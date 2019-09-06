d012b0432492f9d640335bfa0b1f174b1f9fa001
Livia de Castro e Mello <lcastroemello@gmail.com>
2019-09-06 18:26:38 +0200

    background defined

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener("load", () => {
-            resolve(image);
-        });
-        image.src = url;
+import SpriteSheet from "./SpriteSheet.js";
+import { loadImage, loadLevel } from "./loaders.js";
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
     });
 }
 
@@ -13,6 +16,14 @@ const context = canvas.getContext("2d");
 
 context.fillRect(0, 0, 50, 50);
 
-loadImage("./img/tiles.png").then(image => {
-    context.drawImage(image, 0, 0, 16, 16);
+loadImage("/img/tiles.png").then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define("ground", 0, 0);
+    sprites.define("sky", 3, 23);
+
+    loadLevel("1-1").then(level => {
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, sprites);
+        });
+    });
 });
