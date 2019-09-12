a1fb721e6107f51cd4968d86f1bfc101106bf3d7
Livia de Castro e Mello <lcastroemello@gmail.com>
2019-09-12 20:34:17 +0200

    background defined as grid, preparation for collision

---
+++ public/js/TileCollider.js
@@ -0,0 +1,14 @@
+import TileResolver from "./TileResolver.js";
+
+export default class TileCollider {
+    constructor(tileMatrix) {
+        this.tiles = new TileResolver(tileMatrix);
+    }
+
+    test(entity) {
+        const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
+        if (match) {
+            console.log("Matched tile", match, match.tile);
+        }
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -11,40 +11,43 @@ import Keyboard from "./KeyboardState.js";
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel("1-1")]).then(
-    ([mario, backgroundSprites, level]) => {
-        const comp = new Compositor();
-        const backgroundLayer = createBackgroundLayer(
-            level.backgrounds,
-            backgroundSprites
-        );
-        comp.layers.push(backgroundLayer);
+Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
+    console.log("testing level", level);
 
-        const gravity = 2000;
-        mario.pos.set(64, 189);
+    const gravity = 2000;
+    mario.pos.set(64, 64);
 
-        const SPACE = 32;
-        const input = new Keyboard();
-        input.addMapping(SPACE, keyState => {
-            if (keyState) {
-                mario.jump.start();
-            } else {
-                mario.jump.cancel();
+    level.entities.add(mario);
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+        console.log(keyState);
+    });
+    input.listenTo(window);
+
+    // debugger (where is Mario?)
+    ["mousedown", "mousemove"].forEach(eventName => {
+        canvas.addEventListener(eventName, event => {
+            if (event.buttons === 1) {
+                mario.vel.set(0, 0);
+                mario.pos.set(event.offsetX, event.offsetY);
             }
-            console.log(keyState);
         });
-        input.listenTo(window);
+    });
+    /////////
 
-        const spriteLayer = createSpriteLayer(mario);
-        comp.layers.push(spriteLayer);
+    const timer = new Timer(1 / 60);
+    timer.update = function update(deltaTime) {
+        level.update(deltaTime);
+        level.comp.draw(context);
+        mario.vel.y += gravity * deltaTime;
+    };
 
-        const timer = new Timer(1 / 60);
-        timer.update = function update(deltaTime) {
-            mario.update(deltaTime);
-            comp.draw(context);
-            mario.vel.y += gravity * deltaTime;
-        };
-
-        timer.start();
-    }
-);
+    timer.start();
+});
